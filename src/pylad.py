ada2ff654932639ff0b73bcf3a99914e1982a5e1
yafp <fidel@yafp.de>
2016-10-22 11:37:02 +0200

    20161022 - Adding dropdown to UI ... and several other improvements

--- src/pylad.py
+++ src/pylad.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-1 -*-
 
-"""This is pylad - a python based application launcher"""
+# NAME:         pylad
+# DESCRIPTION:  Python based application launcher
+# AUTHOR:       yafp
+# URL:          https://github.com/yafp/pylad
 
+
+# -----------------------------------------------------------------------------------------------
+# RESOURCES
+# -----------------------------------------------------------------------------------------------
 # General App tutorial:                     http://sebsauvage.net/python/gui/#our_project
+# Tkinter:                                  https://docs.python.org/2/library/tkinter.html
 # Dropdown:                                 https://www.reddit.com/r/learnpython/comments/2wn1w6/how_to_create_a_drop_down_list_using_tkinter/
+# Search icon:                              http://fontawesome.io/icon/search/
+# dropdown for results                      https://www.tutorialspoint.com/python/tk_menubutton.htm
+
+
+
+
+# -----------------------------------------------------------------------------------------------
+# TODO
+# -----------------------------------------------------------------------------------------------
+# - global hotkey
+# - remember last window position                       https://wiki.tcl-lang.org/14452
 
 
 # -----------------------------------------------------------------------------------------------
@@ -12,24 +31,28 @@
 # -----------------------------------------------------------------------------------------------
 try:
     # Python2
-    import Tkinter as tk
+    import Tkinter as tk            # for the UI
 except ImportError:
     # Python3
-    import tkinter as tk
+    import tkinter as tk            # for the UI
 
-
-
-#import Tkinter                      # for the UI
 import os, fnmatch                  # for searching applications
 from subprocess import call         # for calling external commands
-import subprocess
+import subprocess                   # for checking if cmd_exists
+
+
+# dropdown
+#from Tkinter import *
+#import Tkinter as ttk
+#from ttk import *
 
 
 # -----------------------------------------------------------------------------------------------
 # CONFIG
 # -----------------------------------------------------------------------------------------------
 appName = "pylad"
-appVersion = "20161020.01"
+appVersion = "20161022.01"
+appURL = "https://github.com/yafp/pylad"
 
 debug = True                    # True or False
 #debug = False                    # True or False
@@ -40,8 +63,9 @@ debug = True                    # True or False
 # CODE
 # -----------------------------------------------------------------------------------------------
 
-# via: http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python/377028
+# Check if command exists
 #
+# via: http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python/377028
 def cmd_exists(cmd):
     printDebugToTerminal('Executing method cmd_exists')
     return subprocess.call("type " + cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
@@ -51,7 +75,6 @@ def cmd_exists(cmd):
 def printDebugToTerminal(str):
     if(debug == True):
         print ("debug >> "+str)
-
 
 
 class simpleapp_tk(tk.Tk):
@@ -64,21 +87,22 @@ class simpleapp_tk(tk.Tk):
         self.grid()
 
         # Search field
+        #
         self.entryVariable = tk.StringVar()
         self.entry = tk.Entry(self,textvariable=self.entryVariable)
-        self.entry.grid(column=0,row=0,sticky='EW')
-    
+        self.entry.grid(row=0, column=0, padx=5, pady=0,sticky='EW')            # set padding
+        self.entry.config(background="white")                       # set background color
+        self.entry.config(foreground="gray")                       # set font color
+        self.entry.config(highlightbackground="gray")               # set border color
+        #
         self.entry.bind("<Return>", self.OnPressEnter)
         #self.entry.bind("<Return>", self.launchExternalApp)
-        
-        self.entry.grid(row=0, column=0, padx=5, pady=0)            # set padding
-        self.entry.config(background="white")                       # set background color
-        self.entry.config(foreground="black")                       # set font color
-        self.entry.config(highlightbackground="gray")               # set border color
-        
-        self.entry.bind('<Key>', self.OnPressKey)                   # on keypress
+        self.entry.bind('<Down>', self.OnArrowDown)                 # on Arrow Down
+        #self.entry.bind('<Key>', self.OnPressKey)                   # on keypress
         self.entry.bind('<KeyRelease>', self.getSearchString)       # on keypress release
         self.entry.bind('<Escape>', self.OnPressESC)                # on ESC
+
+        #
         self.entryVariable.set(u"")                                 # set content on start = empty
 
         # Launch button
@@ -89,29 +113,39 @@ class simpleapp_tk(tk.Tk):
         self.button['state'] = 'disabled'
         self.button.grid(column=1,row=0)
 
+        # Dropdown (featuring results from search)
+        #
+        #optionList = ('Result', 'Option 2', 'Option 3')
+        optionList = (' ')
+        self.v = tk.StringVar()
+        #self.v.set(optionList[0]) # select 1 item of list
+        self.om = tk.OptionMenu(self, self.v, *optionList)
+        self.om.grid(row=1, column=0, padx=5, pady=0,sticky='EW')
+        self.om.bind('<Escape>', self.OnPressESC)                # on ESC
+
         # Status label
         self.labelVariable = tk.StringVar()
-        
-        self.label = tk.Label(self,textvariable=self.labelVariable, anchor="w",fg="gray",bg="lightgray")
-        #self.label.grid(row=0, column=0, padx=5, pady=0)            # set padding
-        self.label.grid(column=0,row=1,columnspan=2,sticky='EW',padx=5, pady=0)
+        self.label = tk.Label(self,textvariable=self.labelVariable, anchor="center",fg="gray")
+        self.label.grid(row=1, column=1, sticky='EW', padx=5, pady=0)
         self.labelVariable.set(u"") # set empty text
-        
-        # Version label        
+
+        # Version label
         self.labelVersionVariable = tk.StringVar()
-        self.labelVersion = tk.Label(self,textvariable=self.labelVersionVariable, anchor="center",fg="gray",bg="lightgray")
+        self.labelVersion = tk.Label(self,textvariable=self.labelVersionVariable, anchor="center",fg="gray")
         self.labelVersion.grid(column=0,row=2,columnspan=2,sticky='EW')
         self.labelVersionVariable.set("r"+appVersion)
+        self.labelVersionVariable.set(appURL +" - r"+appVersion)
+
 
         # misc
         self.grid_columnconfigure(0,weight=1)
-        self.resizable(True,False) # make it resizeable in width only - not in height
+        #self.resizable(True,False) # make it resizeable in width only - not in height
+        self.resizable(False,False) # make it un-resizeable
         self.update()
-        
+
         # Min size: http://stackoverflow.com/questions/10448882/how-do-i-set-a-minimum-window-size-in-tkinter
         self.minsize(self.winfo_width()+200, self.winfo_height())
-        
-        
+
         self.geometry(self.geometry())
         self.entry.focus_set()
         self.entry.selection_range(0, tk.END)
@@ -127,45 +161,51 @@ class simpleapp_tk(tk.Tk):
             self.resetUI()
             return;
         else:
+            print ('\n')
             printDebugToTerminal('Searching matching application for: '+str)
             searchResults = fnmatch.filter(os.listdir('/usr/bin'), '*'+str+'*')     # search for executables featuring *str*
-            
-            numberOfSearchResults = len(searchResults)  #
-            
-            self.labelVariable.set(numberOfSearchResults)
-            if(numberOfSearchResults == 1):     # got 1 hit
-                printDebugToTerminal('Got 1 hit, autocompleting search field')
+
+
+            self.labelVariable.set(len(searchResults)) # show number of search results in status label
+            if(len(searchResults) == 1):     # if we got 1 search-results
                 printDebugToTerminal(searchResults[0])
-                
-                self.label.config(fg='green',bg="lightgray")        # Change color of status label
+                printDebugToTerminal('Autocompleting search field')
+
+                self.label.config(fg='green',bg="lightgray")    # Change color of status label
                 self.button['state'] = 'normal'                 # enabling Launch Button
-                
+
+                # search field
                 self.entry.config(background="white")           # set background color
                 self.entry.config(foreground="green")           # set font color
-                self.entry.config(highlightbackground="green")   # set border color
-                self.entryVariable.set(searchResults[0])        # Display name of executable in search field 
+                self.entry.config(highlightbackground="green")  # set border color
+                #self.entryVariable.set(searchResults[0])        # Display name of executable in search field
                 self.entry.focus_set()
                 #self.entry.selection_range(0, tk.END)      # select entire field content
-                
-                 # set cursor to end
-                # MISSING
+                self.entry.icursor(tk.END)                      # set cursor to end
 
             else:           # got several hits
-                printDebugToTerminal('Got several possible application matches')
+                printDebugToTerminal('Results:')
                 self.entry.config(background="white")           # set background color
-                self.entry.config(foreground="black")           # set font color
+                self.entry.config(foreground="gray")            # set font color
                 self.entry.config(highlightbackground="red")    # set border color
                 self.button['state'] = 'disabled'               # disabling Launch Button
                 self.label.config(fg='red',bg="lightgray")
-            
-            print(numberOfSearchResults)
+
+            # Update Dropdown
+            #optionList = ('one', 'two', 'three')
+            optionList = searchResults
+            self.om = tk.OptionMenu(self, self.v, *optionList)
+            self.om.grid(row=1, column=0, padx=5, pady=0, sticky='EW')
+            self.v.set(optionList[0])
+
+            print(len(searchResults))
             print(searchResults)
             return;
 
 
     # On Pressing button
     def OnButtonClick(self):
-        self.labelVariable.set( self.entryVariable.get()+" (Clicked the button)" )
+        #self.labelVariable.set( self.entryVariable.get()+" (Clicked the button)" )
         printDebugToTerminal('----------- Clicked the button ------------')
         self.launchExternalApp()                                          # reset the UI
 
@@ -173,27 +213,30 @@ class simpleapp_tk(tk.Tk):
     # launching external application
     def launchExternalApp(self):
         printDebugToTerminal('launching external application')
-        self.labelVariable.set( self.entryVariable.get()+" (Clicked the button)" )
-        
-        checkExecutable = cmd_exists(self.entryVariable.get())                  # check if name exists and is executable
-        if (checkExecutable == True):
-            call([self.entryVariable.get(), ""])                    # launch external command
-            self.resetUI()                                          # reset the UI
+        #self.labelVariable.set( self.entryVariable.get()+" (Clicked the button)" )
+        selectedApplicationName = self.v.get()          # get value of OptionMenu
+        if selectedApplicationName != "":
+            checkExecutable = cmd_exists(selectedApplicationName)                  # check if name exists and is executable
+            if (checkExecutable == True):
+                #call([self.entryVariable.get(), ""])                    # launch external command
+                call([selectedApplicationName, ""])                    # launch external command
+                self.resetUI()                                          # reset the UI
+            else:
+                print ('ERROR >> Checking the executable failed....')
         else:
-            print "ERROR >> Checking the executable failed...."
-
+            printDebugToTerminal('No application selected')
 
     # On Pressing ENTER
     def OnPressEnter(self,event):
-        self.labelVariable.set( self.entryVariable.get()+" (Pressed ENTER)" )
+        #self.labelVariable.set( self.entryVariable.get()+" (Pressed ENTER)" )
         printDebugToTerminal('----------- Pressed Enter ------------')
         self.launchExternalApp()
 
 
     # On KeyPress in search field
-    def OnPressKey(self, event):
-        #print('Last Keypress:\t {k!r}'.format(k = event.char))     # output last keypress event 
-        printDebugToTerminal('Last Keypress:\t {k!r}'.format(k = event.char))  # output last keypress event 
+    #def OnPressKey(self, event):
+        #print('Last Keypress:\t {k!r}'.format(k = event.char))     # output last keypress event
+        #printDebugToTerminal('Last Keypress:\t {k!r}'.format(k = event.char))  # output last keypress event
 
 
     # On ESC
@@ -205,47 +248,44 @@ class simpleapp_tk(tk.Tk):
 
     # update current search string and trigger app search
     def getSearchString(self, event):
-        curString = self.entryVariable.get()
-        printDebugToTerminal('Search-String:\t '+curString)         # output current content of search field
-        self.searchingApplication(curString)
+        if event.char != '': # if not ARRAY DOWN then
+            curString = self.entryVariable.get()                        # get current search string
+            #printDebugToTerminal('Search-String:\t '+curString)         # output current content of search field
+            self.searchingApplication(curString)                        # start search for this string
+
+
+    # Arrow Down in Search field should list
+    def OnArrowDown(self, event):
+        print ("Arrow down pressed - should option Option list and set focus to it")
+        self.om.focus_set()
+        self.event_generate('<space>') # - Open the OptionMenu by simulating a keypress
+        return;
 
 
     # reset the UI back to default
     def resetUI(self):
-        print "\n"
+        print ("\n")
         printDebugToTerminal('Resetting UI')
         # search field
-        self.entry.config(background="white")
-        self.entry.config(foreground="black")
+        self.entry.config(background="white")                       # set background color
+        self.entry.config(foreground="gray")                        # set foreground color
         self.entry.config(highlightbackground="gray")               # set border color
         self.entryVariable.set(u"")                                 # Empty search
-        self.entry.focus_set()
+        self.entry.focus_set()                                      # set focus to search field
         self.entry.selection_range(0, tk.END)
+        # dropdown
+        optionList = (' ')
+        self.om = tk.OptionMenu(self, self.v, *optionList)
+        self.om.grid(row=1, column=0, padx=5, pady=0, sticky='EW')
+        self.v.set(optionList[0])                                   # select 1 menu-item
         # button
-        self.button['state'] = 'disabled'
+        self.button['state'] = 'disabled'                           # disable launch-button
         # status label
         self.labelVariable.set("")                                  # empty status label
-        self.label.config(fg='grey',bg="lightgray")                 # reset status label colors
+        self.label.config(fg='gray',bg="lightgray")                 # reset status label colors
 
 
 if __name__ == "__main__":
     app = simpleapp_tk(None)
     app.title(appName)
-    
-    
-    # WINDOW POSITION AND SIZE
-    #
-    #w = 400 # width for the Tk root
-    #h = 150 # height for the Tk root
-    #ws = app.winfo_screenwidth() # width of the screen
-    #hs = app.winfo_screenheight() # height of the screen
-    #
-    # calculate x and y coordinates for the Tk root window
-    #x = (ws/2) - (w/2)
-    #y = (hs/2) - (h/2)
-    #
-    #app.geometry('%dx%d+%d+%d' % (w, h, x, y))
-    #app.geometry(app.geometry())
-    
-    
     app.mainloop()
