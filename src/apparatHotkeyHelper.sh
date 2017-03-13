a59c2051145afc35f885d2ee012ae054f5af598e
yafp <fidel@yafp.de>
2017-03-13 20:17:30 +0100

    20170313 - Optimizing hotkey helper

--- src/apparatHotkeyHelper.sh
+++ src/apparatHotkeyHelper.sh
@@ -2,31 +2,41 @@
 
 # Function:
 #   checks if apparat.py is already running
-#   if it is - the apparat window gets focus
-#   if not - apparat.py gets started
+#   if it is running: the apparat window gets focus if it isnt active already. If it is active it gets minimized
+#   if it is not runnung: apparat.py gets started
 #
 # Usage:
 #   should be configured to be launched via a global hotkey using system-settings
 #
+# Requirements:
+# - xdotool
+# - wmctrl
+
 
 # CONFIG
 #
 APPARAT_FOLDER=$HOME'/Dropbox/Temp/apparat/src/'
 APPARAT_EXECUTABLE='apparat.py'
 
+
 # MAIN
 #
 wmctrl -lx | awk '{print $3}' | grep -i "$APPARAT_EXECUTABLE" # check if apparat.py is running right now
 
-if [ $? -eq 0 ]; then # it is - so focus it
-    #notify-send "Focusing apparat" -t 3000
-    #sleep 1
-    wmctrl -xa $APPARAT_EXECUTABLE
-    exit 1
+if [ $? -eq 0 ]; then # it is running
 
-else # it isnt - so start it
+    ACTIVE_APP_WINDOW_TITLE=$(xdotool getwindowfocus getwindowname) # check active app
+    if [ $ACTIVE_APP_WINDOW_TITLE == 'Apparat' ]; then 
+        xdotool windowminimize $(xdotool getactivewindow) # apparat is active already - minimize it
+        
+    else # focus it
+        #sleep 1
+        wmctrl -xa $APPARAT_EXECUTABLE
+        exit 1
+    fi
+
+else # apparat is not running -> start it
     #notify-send "Starting: "$APPARAT_EXECUTABLE -t 3000
-    #sleep 1
     cd "$APPARAT_FOLDER" && python "./apparat.py"
     exit 0
 fi
