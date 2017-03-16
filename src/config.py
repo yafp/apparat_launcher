a3d5c75f13a2f0ee8731cbea2acf6f40db903bd5
yafp <fidel@yafp.de>
2017-03-16 20:11:26 +0100

    20170316 - Moving some parts of apparat.py to seperate py files

---
+++ src/config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+"""apparat - an application launcher for linux"""
+
+
+# -----------------------------------------------------------------------------------------------
+# CONFIG (DEVELOPER)
+# -----------------------------------------------------------------------------------------------
+APP_VERSION = '20170316.01'
+WINDOW_WIDTH = 350 #350
+WINDOW_HEIGHT = 300
+TARGET_ICON_SIZE = 128
+TRANSPARENCY_VALUE = 255                # app TRANSPARENCY_VALUE - Values: 0-255
---
+++ src/constants.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+"""apparat - an application launcher for linux"""
+
+import os                           # for searching applications and file/folder/path handling
+
+# -----------------------------------------------------------------------------------------------
+# CONSTANTS (DEVELOPER)
+# -----------------------------------------------------------------------------------------------
+APP_NAME = 'Apparat'
+APP_URL = 'https://github.com/yafp/apparat'
+APP_LICENSE = 'GPL3'
+APP_TRAY_TOOLTIP = 'apparat'
+APP_TRAY_ICON = 'gfx/core/bt_appIcon_16.png'
+APP_INI_FOLDER = os.environ['HOME']+'/.config/apparat/'
+APP_INI_PATH = APP_INI_FOLDER+'apparat.ini'
+APP_PLUGINS_INTERNET_SEARCH_TRIGGER = ('!a', '!b', '!e', '!g', '!l', '!o', '!r', '!s', '!t', '!v', '!w', '!y') # must be tuple
+APP_PLUGINS_SESSION_TRIGGER = ('!hibernate', '!sleep', '!lock', '!logout', '!reboot', '!restart', '!shutdown')
