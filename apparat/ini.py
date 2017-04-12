a25cd201035d05d9f3a292bef782ba4c2b5f5eef
yafp <fidel@yafp.de>
2017-04-12 23:22:05 +0200

    20170412 - fixing #13 - validating ini

--- apparat/ini.py
+++ apparat/ini.py
@@ -8,7 +8,7 @@
 
 # general
 import os
-import ConfigParser                 # to handle .ini/configuration files
+import ConfigParser # to handle .ini/configuration files
 
 ## apparat
 import constants
@@ -22,19 +22,18 @@ import tools
 def read_single_value(section_name, key_name):
     """Method to read a single value from the configuration file apparat.ini"""
     check_if_ini_exists()
-    tools.debug_output('read_single_value', 'starting')
+    tools.debug_output('read_single_value', 'starting', 0)
     config = ConfigParser.ConfigParser()
     config.read(constants.APP_INI_PATH)
-    #print config.sections()
     try:
         value = config.get(section_name, key_name)
-        tools.debug_output('read_single_value', 'Section: '+section_name)
-        tools.debug_output('read_single_value', 'Key: '+key_name)
-        tools.debug_output('read_single_value', 'Value: '+value)
+        tools.debug_output('read_single_value', 'Section: '+section_name, 1)
+        tools.debug_output('read_single_value', 'Key: '+key_name, 1)
+        tools.debug_output('read_single_value', 'Value: '+value, 1)
     except ConfigParser.ParsingError:
         # Issue: #13 - Create key if it doesnt exist already
         print('read_single_ini_value', 'key '+key_name+' does not exist. Should create the key with a default value in this case - see #13')
-        tools.debug_output('read_single_value', 'key '+key_name+' does not exist. Should create the key with a default value in this case - see #13')
+        tools.debug_output('read_single_value', 'key '+key_name+' does not exist. Should create the key with a default value in this case - see #13', 2)
         if(key_name == 'apparat_started') or (key_name == 'command_executed') or (key_name == 'plugin_executed'):
             value = '0'
         elif (key_name == 'hide_ui_after_command_execution'):
@@ -49,32 +48,36 @@ def read_single_value(section_name, key_name):
 def write_single_value(section_name, key_name, value):
     """Method to write a single value to the configuration file apparat.ini"""
     check_if_ini_exists()
-    tools.debug_output('write_single_value', 'starting')
-    config = ConfigParser.ConfigParser()
-    config.read(constants.APP_INI_PATH)
-    config.set(section_name, key_name, value)
-    tools.debug_output('write_single_value', 'Section: '+section_name)
-    tools.debug_output('write_single_value', 'Key: '+key_name)
-    tools.debug_output('write_single_value', 'Value: '+str(value))
-    with open(constants.APP_INI_PATH, 'wb') as configfile:
-        config.write(configfile)
+    tools.debug_output('write_single_value', 'starting', 0)
+    try:
+        config = ConfigParser.ConfigParser()
+        config.read(constants.APP_INI_PATH)
+        config.set(section_name, key_name, value)
+        tools.debug_output('write_single_value', 'Section: '+section_name, 1)
+        tools.debug_output('write_single_value', 'Key: '+key_name, 1)
+        tools.debug_output('write_single_value', 'Value: '+str(value), 1)
+        with open(constants.APP_INI_PATH, 'wb') as configfile:
+            config.write(configfile)
+    except ConfigParser.NoSectionError:
+        tools.debug_output('write_single_value', 'Section '+section_name+' does not exist.', 3)
+        return
 
 
 def check_if_ini_exists():
     """Method to check if an ini file exists - and generate it if it doesnt"""
-    tools.debug_output('check_if_ini_exists', 'starting')
+    tools.debug_output('check_if_ini_exists', 'Start checking for ini file', 0)
 
     ## check if config folder exists - if not create it
     if not os.path.exists(constants.APP_INI_FOLDER):
-        tools.debug_output('check_if_ini_exists', 'Creating config folder')
+        tools.debug_output('check_if_ini_exists', 'Creating config folder', 2)
         os.makedirs(constants.APP_INI_FOLDER)
 
     ## check if config file exists
     if os.path.isfile(constants.APP_INI_PATH):
-        tools.debug_output('check_if_ini_exists', 'Found ini file')
+        tools.debug_output('check_if_ini_exists', 'Found ini file ('+constants.APP_INI_PATH+')', 1)
     else:
-        tools.debug_output('check_if_ini_exists', 'No ini file found')
-        tools.debug_output('check_if_ini_exists', 'Creating default ini file')
+        tools.debug_output('check_if_ini_exists', 'No ini file found', 2)
+        tools.debug_output('check_if_ini_exists', 'Creating default ini file', 2)
         mode = 'a' if os.path.exists(constants.APP_INI_PATH) else 'w'
         with open(constants.APP_INI_PATH, mode) as f:
             f.write('[Language]\n')
@@ -85,10 +88,54 @@ def check_if_ini_exists():
             f.write('apparat_started = 0\n')
             f.write('command_executed = 0\n')
             f.write('plugin_executed = 0\n')
-        tools.debug_output('check_if_ini_exists', 'Finished ini file creation')
-    tools.debug_output('check_if_ini_exists', 'finished')
+        tools.debug_output('check_if_ini_exists', 'Finished ini file creation', 1)
 
 
 def validate():
     """Validate the entire ini"""
-    tools.debug_output('validate', 'Dummy: Validate ini')
+    check_if_ini_exists()
+    # https://gist.github.com/tsuriga/5bc5bbfaf21c6a51cda7
+    # https://pymotw.com/3/configparser/
+    tools.debug_output('validate', 'Validating ini ('+constants.APP_INI_PATH+') for existing sections and options', 0)
+
+    # Section: Language
+    #
+    SECTIONS = ['Language']
+    OPTIONS = ['lang']
+    validate_single_section(SECTIONS, OPTIONS)
+
+    # Section: General
+    #
+    SECTIONS = ['General']
+    OPTIONS = ['hide_ui_after_command_execution']
+    validate_single_section(SECTIONS, OPTIONS)
+
+    # Section: Statistics
+    #
+    SECTIONS = ['Statistics']
+    OPTIONS = ['apparat_started', 'command_executed', 'plugin_executed']
+    validate_single_section(SECTIONS, OPTIONS)
+
+    tools.debug_output('validate', 'Finished validating complete ini ('+constants.APP_INI_PATH+')', 0)
+
+
+def validate_single_section(sections, options):
+    "Validates a given ini section for a defined amount of options"
+    tools.debug_output('validate_single_section', 'Validating ini ('+constants.APP_INI_PATH+') for section'+str(sections), 0)
+    config = ConfigParser.ConfigParser()
+    config.read(constants.APP_INI_PATH)
+
+    for section in sections:
+        # sections
+        has_section = config.has_section(section)
+        if has_section is True:
+            tools.debug_output('validate_single_section', '{} section exists: {}'.format(section, has_section), 1)
+        else:
+            tools.debug_output('validate_single_section', '{} section is missing: {}'.format(section, has_section), 3)
+        # options
+        for candidate in options:
+            has_option = config.has_option(section, candidate)
+            if has_option is True:
+                tools.debug_output('validate_single_section', '{}.{:<12}  : {}'.format(section, candidate, has_option), 1)
+            else:
+                tools.debug_output('validate_single_section', '{}.{:<12}  : {}'.format(section, candidate, has_option), 3)
