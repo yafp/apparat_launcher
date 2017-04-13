3cd359c36f224f47cc7c0975aa000debbbb904cb
yafp <fidel@yafp.de>
2017-04-13 20:15:41 +0200

    20170413 - Fixing #35

--- apparat/plugin_misc.py
+++ apparat/plugin_misc.py
@@ -2,7 +2,7 @@
 """apparat - plugin: search-internet"""
 
 ## general
-
+import os
 import wx
 
 ## apparat
@@ -26,12 +26,15 @@ def prepare_general(current_search_string, main_window):
     """Prepare General"""
     tools.debug_output('prepare_general', 'starting', 1)
 
+    # Reset status notification back to OK
+    main_window.status_notification_reset()
+
     if  current_search_string.startswith('!open'):
         tools.debug_output('prepare_general', 'Case: Open', 1)
         prepare_plugin_misc_open(main_window)
         return
 
-    if  current_search_string == '!help':
+    if  current_search_string == '!help': # opens online documentation of apparat
         tools.debug_output('prepare_general', 'Case: Help', 1)
         main_window.open_app_url()
         # hide UI
@@ -42,7 +45,7 @@ def prepare_general(current_search_string, main_window):
         main_window.reset_ui()
         return
 
-    if  current_search_string == '!preferences' or current_search_string == '!prefs':
+    if  current_search_string == '!preferences' or current_search_string == '!prefs': # opens apparat preferences
         tools.debug_output('prepare_general', 'Case: Preferences', 1)
         main_window.open_preference_window()
         main_window.reset_ui()
@@ -50,14 +53,14 @@ def prepare_general(current_search_string, main_window):
 
     else:
         tools.debug_output('prepare_general', 'Error: Unexpected misc plugin command', 3)
-        main_window.display_error_notification('Unexpected misc plugin command')
+        main_window.status_notification_display_error('Unexpected misc plugin command')
         return
 
     tools.debug_output('prepare_general', 'finished', 1)
 
 
 def prepare_plugin_misc_open(main_window):
-    """Plugin Misc - Open"""
+    """Plugin Misc - Open - Opens file or location using xdg-open"""
     tools.debug_output('prepare_plugin_misc_open', 'starting', 1)
     main_window.plugin__update_general_ui_information('Misc (Open)') ## update plugin info
 
@@ -73,7 +76,12 @@ def prepare_plugin_misc_open(main_window):
         main_window.ui__bt_selected_parameter.SetBitmap(main_window.ui__bt_selected_parameter_img.ConvertToBitmap())
 
         ## set parameter
-        main_window.ui__txt_selected_parameter.SetValue(main_window.ui__cb_search.GetValue()[6:]) # possible parameter
+        if (main_window.ui__cb_search.GetValue()[6:7] == '~'):
+            tools.debug_output('prepare_plugin_misc_open', 'Replacing ~', 1)
+            home = os.environ['HOME']
+            main_window.ui__txt_selected_parameter.SetValue(home+main_window.ui__cb_search.GetValue()[7:]) # possible parameter
+        else:
+            main_window.ui__txt_selected_parameter.SetValue(main_window.ui__cb_search.GetValue()[6:]) # possible parameter
 
         ## set command
         main_window.ui__txt_selected_app.SetValue('xdg-open')
